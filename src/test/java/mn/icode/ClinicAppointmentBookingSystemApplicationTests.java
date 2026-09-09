9d10cec54582d53c8ee012dd8dc8cf3b8bb4e3e2
Kira Gami <hishgee.kira@gmail.com>
2026-09-09 20:45:30 +0800

    feat: initialize Spring Boot project with PostgreSQL configuration (#1)

---
+++ src/test/java/mn/icode/ClinicAppointmentBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package mn.icode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicAppointmentBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
