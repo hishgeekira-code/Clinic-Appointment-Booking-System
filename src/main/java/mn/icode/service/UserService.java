2f1b353e5738366dbf18ea35530772b24a7f1734
Kira Gami <hishgee.kira@gmail.com>
2026-09-10 19:54:58 +0800

    feat: implement user registration and login authentication

---
+++ src/main/java/mn/icode/service/UserService.java
@@ -0,0 +1,38 @@
+package mn.icode.service;
+
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import mn.icode.dto.RegisterDto;
+import mn.icode.entity.User;
+import mn.icode.model.Role;
+import mn.icode.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void registerUser(RegisterDto dto) {
+        if (userRepository.existsByEmail(dto.getEmail())) {
+            throw new RuntimeException("Email аль хэдийн бүртгэгдсэн байна!");
+        }
+
+        User user = new User();
+        user.setFirstName(dto.getFirstName());
+        user.setLastName(dto.getLastName());
+        user.setEmail(dto.getEmail());
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        user.setPhone(dto.getPhone());
+        user.setRole(Role.CUSTOMER);
+
+        userRepository.save(user);
+    }
+}
